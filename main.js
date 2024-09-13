162c66c41cc7570a1552c4cb0dc682429ae8cdb0
mdrobinhossen592 <mdrobinhossen592@gmail.com>
2024-09-14 01:06:07 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+
+// নেভিগেশন বাটনের ক্লিক হ্যান্ডলার
+$('.carousel-control-prev-1').click(function() {
+    $('#myCarousel').carousel('prev');
+});
+
+$('.carousel-control-prev-2').click(function() {
+    $('#myCarousel').carousel('prev');
+});
+
+$('.carousel-control-prev-3').click(function() {
+    $('#myCarousel').carousel('prev');
+});
+
+
